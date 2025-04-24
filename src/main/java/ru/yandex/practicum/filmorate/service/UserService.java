250eb35dbd9b70efdbc2ca6b5cbf8d6f68d2a6a1
Sergey Ermolaev <weekendarti@yandex.ru>
2025-04-24 22:51:18 +0300

    ТЗ 11

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
